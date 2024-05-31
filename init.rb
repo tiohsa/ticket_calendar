4153d2fa055f7fcaaadfd74869c65d7e218a66e5
glorydays <glorydays11@gmail.com>
2024-05-31 22:55:19 +0900

    add README

--- init.rb
+++ init.rb
@@ -1,11 +1,11 @@
 Redmine::Plugin.register :ticket_calendar do
   name 'Ticket Calendar plugin'
-  author 'Author name'
+  author 'tiohsa'
   description 'This plugin displays tickets in a calendar view with drag-and-drop functionality.'
   version '0.0.1'
-  url 'http://example.com/path/to/plugin'
-  author_url 'http://example.com/about'
+  url 'https://github.com/tiohsa/ticket_calendar'
+  author_url 'https://github.com/tiohsa'
 
-  permission :view_ticket_calendar, { ticket_calendars: [:index, :update_dates, :update_done_ratio] }, public: true
+  permission :view_ticket_calendar, { ticket_calendars: [:index, :update_dates] }, public: true
   menu :project_menu, :ticket_calendar, { controller: 'ticket_calendars', action: 'index' }, caption: 'Calendar', after: :activity, param: :project_id
 end
